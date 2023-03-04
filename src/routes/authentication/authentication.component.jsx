651013973e971d95653dd030daf9e2abcd2fd543
Mohamed Elkady <mohameda.elqady@gmail.com>
2023-03-05 00:34:45 +0200

    edits with auth page

--- src/routes/authentication/authentication.component.jsx
+++ src/routes/authentication/authentication.component.jsx
@@ -1,24 +1,11 @@
 import "./authentication.styles.scss";
 import SignUpForm from "../../components/sign-up-form/sign-up-form.component";
 import SignInForm from "../../components/sign-in-form/sign-in-form.component";
-import { Fragment } from "react";
-
-import { useSelector } from "react-redux";
-import { selectCurrentUser } from "../../store/user/user.selector";
-
 const Authentication = () => {
-  const currentUser = useSelector(selectCurrentUser);
-
   return (
     <div className="authentication-container">
-      {currentUser ? (
-        <h1>Welcome back {currentUser.displayName? currentUser.displayName : "User"}</h1>
-      ) : (
-        <Fragment>
-          <SignInForm />
-          <SignUpForm />
-        </Fragment>
-      )}
+      <SignInForm />
+      <SignUpForm />
     </div>
   );
 };
